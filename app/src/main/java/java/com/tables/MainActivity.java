2ea4c583defe484f63d5786844e258a3082cd38a
navneetpurohit <navnetpurohit1998@gmail.com>
2020-08-24 08:10:26 +0530

    Final Commit

---
+++ app/src/main/java/java/com/tables/MainActivity.java
@@ -0,0 +1,24 @@
+package java.com.tables;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+   }
